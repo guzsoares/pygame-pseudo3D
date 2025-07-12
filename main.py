b372686e5db6d75e5526afc2cddf0291ea25af08
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2025-07-12 09:02:58 -0300

    Add NPC pathfinding, health UI, and game over logic
    Introduces a pathfinding system for NPC movement and updates NPC logic to use pathfinding for chasing the player. Adds player health tracking, a blood screen effect on damage, and a game over screen with restart logic. Implements a digit-based health display and includes new textures for UI elements and digits.

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ from sprite_object import *
 from object_handler import *
 from weapon import *
 from sound import *
+from pathfinding import *
 
 class Game:
     def __init__(self):
         pg.init()
         pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RESOLUTION)
+        pg.event.set_grab(True)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -25,6 +30,7 @@ class Game:
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
         self.object_handler = ObjectHandler(self)
+        self.pathfinding = PathFinding(self)
         self.weapon = Weapon(self)
         self.sound = Sound(self)
 
@@ -45,14 +51,13 @@ class Game:
         #self.player.draw()
 
     def check_events(self):
+        self.global_trigger = False
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    pg.quit()
-                    sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     def run(self):
